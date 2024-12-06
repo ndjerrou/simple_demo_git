a16a0f9773851e811d7e632ac61b308981a3c703
ndjerrou <n.djerrou@gmail.com>
2024-12-06 11:15:33 +0100

    Added useState import

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 import localFont from 'next/font/local';
 import styles from '@/styles/Home.module.css';
+import { useState } from 'react';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
